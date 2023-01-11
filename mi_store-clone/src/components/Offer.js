7e6f8c4833624f8f7839edd3ac6a2483b09e1a6d
Abhinav Malhotra <113239170+abhinavmalhotra01@users.noreply.github.com>
2023-01-12 01:17:21 +0530

    Delete mi_store-clone directory

--- mi_store-clone/src/components/Offer.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-const Offer = ({src,link,index}) => {
-  return <a href={link}><img src={src} atl={"${index} offer"}/></a>
-}
-
-export default Offer
--- mi_store-clone/src/components/StarProducts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const StarProducts = () => {
-  return (
-    <div>StarProducts</div>
-  )
-}
-
-export default StarProducts
